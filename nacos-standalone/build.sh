0406eb932f2698cecff6811a5f42e7517453cd58
守望时空33 <yanhanhui2001a@163.com>
2023-06-13 15:51:06 +0800

    update

--- nacos-standalone/build.sh
+++ nacos-standalone/build.sh
@@ -4,7 +4,7 @@ VERSION=$1
 if [ ! $VERSION ]; then
 	echo 请指定版本号！
 else
-	IMAGE=swsk33/kafka-standalone
+	IMAGE=swsk33/nacos-standalone
 	echo 正在构建...
 	docker build -f ./latest/Dockerfile -t $IMAGE:$VERSION --network host --build-arg ALL_PROXY="http://127.0.0.1:7500" ./latest
 	echo 创建latest Tag...
