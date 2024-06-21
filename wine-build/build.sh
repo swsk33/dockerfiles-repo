dedc90e0e445691191e2dbc393934d7e6b44e5fe
守望时空33 <yanhanhui2001a@163.com>
2024-06-21 11:12:11 +0800

    update wine-build

--- wine-build/build.sh
+++ wine-build/build.sh
@@ -6,7 +6,7 @@ if [ ! $VERSION ]; then
 else
 	IMAGE=swsk33/wine-build
 	echo 正在构建...
-	docker build -f ./$VERSION/Dockerfile -t $IMAGE:$VERSION ./$VERSION
+	docker build --network host --build-arg ALL_PROXY="http://127.0.0.1:7500" -f ./$VERSION/Dockerfile -t $IMAGE:$VERSION ./$VERSION
 	echo 创建latest Tag...
 	docker tag $IMAGE:$VERSION $IMAGE
 	echo 构建完成！
