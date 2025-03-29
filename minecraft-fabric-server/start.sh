9ace7e89bcbd7b07fc0377e915f5712f9f68b204
守望时空33 <yanhanhui2001a@163.com>
2025-03-29 15:15:20 +0800

    remove fabric server

--- minecraft-fabric-server/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if [ "$EULA" = "true" ]; then
-	echo "By setting the EULA to TRUE, you are indicating your agreement to Minecraft EULA (https://aka.ms/MinecraftEULA)."
-	echo "eula=true" >eula.txt
-fi
-exec java -Xmx${JVM_MAX} -Xms${JVM_MIN} -XX:+UseZGC -jar /minecraft/fabric-server.jar
